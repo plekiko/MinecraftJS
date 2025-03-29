bf2061a5d1d69ade9db089bb785577e2fba5b066
Plekiko <ddf.jakobs@gmail.com>
2025-03-30 00:38:55 +0100

    Skin wip

--- Server/Classes/player.js
+++ Server/Classes/player.js
@@ -1,11 +1,11 @@
 import { Vector2 } from "./helper.js";
 
 export class Player {
-    constructor({ UUID, name, ws }) {
+    constructor({ UUID, name, ws, skin = "steve" }) {
         this.UUID = UUID;
         this.name = name;
         this.ws = ws;
 
-        this.lookDirection = new Vector2(0, 0);
+        this.skin = skin;
     }
 }
