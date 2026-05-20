eb0bc14a9b46400e6f392d6d3d9d0b1e7d9e9ca6
plekiko <117017540+plekiko@users.noreply.github.com>
2026-05-20 11:32:00 +0200

    Update helper.js

--- Server/Classes/helper.js
+++ Server/Classes/helper.js
@@ -1,10 +1,7 @@
+import { randomUUID } from 'crypto';
+
 export function uuidv4() {
-    return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) =>
-        (
-            c ^
-            (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
-        ).toString(16)
-    );
+    return randomUUID();
 }
 
 export function Vector2(x = 0, y = 0) {
